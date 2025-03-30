0454137c56ceaa6fd946e9c0d6e6982bf4d53dde
santhoshkumar011 <163506687+santhoshkumar011@users.noreply.github.com>
2025-03-30 19:45:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Navbar from "./components/Navbar"
-import AboutArea from "./components/Aboutarea"
+import AboutArea from "./components/AboutArea"
 import Main from "./components/Main"
 import AboutPlot from "./components/AboutPlot"
 import Pricing from "./components/Pricing"
