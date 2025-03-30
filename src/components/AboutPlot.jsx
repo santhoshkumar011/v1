f5e363e6b51998a96bd8a3e72cc27c19469b93a8
Sharankumar2609 <220701264@rajalakshmi.edu.in>
2025-03-30 22:18:30 +0530

    Update AboutPlot.jsx

--- src/components/AboutPlot.jsx
+++ src/components/AboutPlot.jsx
@@ -7,10 +7,10 @@ import PropTypes from 'prop-types'
 const Aboutplot = ({name,index}) => {
   return (
     <section id="AboutPlot">
-      <h2 className="abt-plt">ABOUT PLOT</h2>
+      <h2 className="abt-plt">ABOUT MEGACITY</h2>
       <div className="main-plot">
           <div className="main-plot-content">
-              <p>Codename Parandur at plot is an exquisite plotted development located near international Airport of Chennai, spanning 20 acres and featuring 150 meticulously designed plots. This stunning project is situated between a Chennai-Bengaluru Expressway and peaceful Environment, offering residents an unparalleled living experience that seamlessly merges the beauty of nature with sophisticated modernity.</p>
+              <p>Codename Airport Megacity at plot is an exquisite plotted development located near international Airport of Chennai, spanning 20 acres and featuring 150 meticulously designed plots. This stunning project is situated between a Chennai-Bengaluru Expressway and peaceful Environment, offering residents an unparalleled living experience that seamlessly merges the beauty of nature with sophisticated modernity.</p>
              <button><a href={brochure} download="Parandur-Airport-Brochure.pdf" className="download-btn">
             Download Brochure
                 </a>    
