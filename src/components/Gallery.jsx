7e55fa49c08101b561948cda65801ccb192a5f05
santhoshkumar011 <163506687+santhoshkumar011@users.noreply.github.com>
2025-03-31 20:56:01 +0530

    Update Gallery.jsx

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import "../css/Gallery.css";
 import img1 from "../assets/pic1.jpg";
-import img2 from "../assets/pic2.jpg";
-import img3 from "../assets/pic3.jpg";
-import img4 from "../assets/pic4.jpg";
-import img5 from "../assets/pic5.jpg";
+import img2 from "../assets/investment.jpg";
+import img3 from "../assets/metro.jpg";
+import img4 from "../assets/commercial_hub.jpg";
+import img5 from "../assets/transportation.jpg";
 
 const Gallery = () => {
   const [activeIndex, setActiveIndex] = useState(null);
